b284b1aa84693e0767bc8be1e92531b2f83e95ce
Đinh Hiệp <78436741+dinhhiep214@users.noreply.github.com>
2021-09-29 19:41:13 +0700

    authen filter

--- src/main/java/security/AuthenFilter.java
+++ src/main/java/security/AuthenFilter.java
@@ -1,29 +1,39 @@
 package security;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebFilter("/*")
 public class AuthenFilter implements Filter {
 
     private FilterConfig filterConfig = null;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+        request.setCharacterEncoding("UTF-8");
+        String loginURI = request.getContextPath() + "/login";
+        boolean loggedIn = session != null && session.getAttribute("account") != null;
+        boolean loginRequest = request.getRequestURI().equals(loginURI);
 
+        if (loggedIn || loginRequest) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.sendRedirect(loginURI);
+        }
     }
 
     @Override
     public void destroy() {
-
     }
 }
