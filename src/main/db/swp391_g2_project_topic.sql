9bb20439fa3325316b57b0c784f1c90d24293f47
HangNT <hangnt16499@gmail.com>
2021-10-24 14:16:43 +0700

    Update DB

--- src/main/db/swp391_g2_project_topic.sql
+++ src/main/db/swp391_g2_project_topic.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
 --
 -- Host: localhost    Database: swp391_g2_project
 -- ------------------------------------------------------
--- Server version	8.0.16
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,18 +21,18 @@
 
 DROP TABLE IF EXISTS `topic`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `topic` (
-  `topic_id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
+  `topic_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
   `status` varchar(25) DEFAULT 'pending',
-  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `rate` int(11) DEFAULT '0',
+  `content` varchar(4000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `rate` int DEFAULT '0',
   `created_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `month` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`topic_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `topic` (
 
 LOCK TABLES `topic` WRITE;
 /*!40000 ALTER TABLE `topic` DISABLE KEYS */;
-INSERT INTO `topic` VALUES (1,'Quán ngon Cầu Giấy?','approved','Mọi người cho mình hỏi ở chỗ cầu giấy có quán nào ngon mà rẻ không nhỉ?',50,'2021-10-10 22:33:04','2021-10-10 22:33:04','08/2021'),(2,'Quán ngon giá rẻ?','approved','Có chỗ nào ăn lẩu mà ngon bổ rẻ không ạ?',60,'2021-10-13 22:33:04','2021-10-13 22:33:04','09/2021'),(3,'Quán nhậu chất lượng?','pending','Cho hỏi có quán nhậu nào chất lượng ở khu Bách Khoa không?',0,'2021-10-15 22:33:04','2021-10-15 22:33:04','09/2021'),(4,'Hàng ăn sáng rẻ?','reject','Quanh khu Hai Bà Trưng có những hàng ăn sáng nào chất lượng mà giá cả hợp lý không ạ, mọi người recommend giúp mình với?',0,'2021-10-08 22:33:04','2021-10-08 22:33:04','03/2021'),(5,'Test Topic ','pending','Topic Testt',0,'2021-10-17 08:29:16','2021-10-17 08:29:16','10/2021');
+INSERT INTO `topic` VALUES (1,'Quán ngon Cầu Giấy?','approved','Mọi người cho mình hỏi ở chỗ cầu giấy có quán nào ngon mà rẻ không nhỉ?',50,'2021-10-10 22:33:04','2021-10-10 22:33:04',''),(2,'Quán ngon giá rẻ?','approved','Có chỗ nào ăn lẩu mà ngon bổ rẻ không ạ?',60,'2021-10-13 22:33:04','2021-10-13 22:33:04',''),(3,'Quán nhậu chất lượng?','pending','Cho hỏi có quán nhậu nào chất lượng ở khu Bách Khoa không?',0,'2021-10-15 22:33:04','2021-10-15 22:33:04','07/2021'),(4,'Hàng ăn sáng rẻ?','reject','Quanh khu Hai Bà Trưng có những hàng ăn sáng nào chất lượng mà giá cả hợp lý không ạ, mọi người recommend giúp mình với?',0,'2021-10-08 22:33:04','2021-10-08 22:33:04','');
 /*!40000 ALTER TABLE `topic` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-17 16:28:37
+-- Dump completed on 2021-10-24 14:12:06
--- src/main/db/swp391_g2_project_topic_comment.sql
+++ src/main/db/swp391_g2_project_topic_comment.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
 --
 -- Host: localhost    Database: swp391_g2_project
 -- ------------------------------------------------------
--- Server version	8.0.16
+-- Server version	8.0.27
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,17 +21,20 @@
 
 DROP TABLE IF EXISTS `topic_comment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `topic_comment` (
-  `topic_comment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `topic_id` int(11) NOT NULL,
-  `status` varchar(25) DEFAULT 'inactive',
-  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `rate` int(11) DEFAULT NULL,
-  `created_date` datetime NOT NULL,
-  `updated_date` datetime DEFAULT NULL,
+  `topic_comment_id` int NOT NULL AUTO_INCREMENT,
+  `topic_id` int DEFAULT NULL,
+  `account_id` int DEFAULT NULL,
+  `status` varchar(25) DEFAULT 'active',
+  `content` varchar(4000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `rate` int DEFAULT NULL,
+  `created_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`topic_comment_id`),
   KEY `topic_id` (`topic_id`),
+  KEY `topic_comment_account_ibfk_1` (`account_id`),
+  CONSTRAINT `topic_comment_account_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`),
   CONSTRAINT `topic_comment_ibfk_1` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`topic_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -54,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-17 16:28:38
+-- Dump completed on 2021-10-24 14:12:06
