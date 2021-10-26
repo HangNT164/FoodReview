50beb1383657f8b6331c58245d69399c46f3ddec
HangNT <hangnt16499@gmail.com>
2021-10-26 20:36:23 +0700

    Update code

--- src/main/java/controller/AddTopicReviewerController.java
+++ src/main/java/controller/AddTopicReviewerController.java
@@ -4,6 +4,7 @@ import bean.Topic;
 import dao.TopicDao;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
@@ -14,6 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 
@@ -29,43 +33,66 @@ public class AddTopicReviewerController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-        String title = request.getParameter("title");
-        String content = request.getParameter("content");
-        if (isMultipart) {
-            FileItemFactory factory = new DiskFileItemFactory();
-            ServletFileUpload upload = new ServletFileUpload(factory);
-            try {
-                List items = upload.parseRequest(request);
-                Iterator iterator = items.iterator();
-                while (iterator.hasNext()) {
-                    FileItem item = (FileItem) iterator.next();
-                    if (!item.isFormField()) {
-                        String fileName = item.getName();
-                        String root = getServletContext().getRealPath("/");
-                        File path = new File(root + "/uploads");
-                        if (!path.exists()) {
-                            path.mkdirs();
+        try (PrintWriter out = response.getWriter()) {
+            boolean isMultiPart = ServletFileUpload.isMultipartContent(request);
+            if (!isMultiPart) {
+                System.out.println("Error");
+            } else {
+                FileItemFactory factory = new DiskFileItemFactory();
+                ServletFileUpload upload = new ServletFileUpload(factory);
+                List items = null;
+                String lsFileName = "";
+
+                try {
+                    items = upload.parseRequest(request);
+                } catch (FileUploadException e) {
+                    System.err.println(e.getMessage());
+                }
+
+                Iterator iter = items.iterator();
+                Hashtable params = new Hashtable();
+                while (iter.hasNext()) {
+                    FileItem item = (FileItem) iter.next();
+                    if (item.isFormField()) {
+                        // convert to UTF-8
+                        params.put(new String(item.getFieldName().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8),
+                                new String(item.getString().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8));
+                    } else {
+                        try {
+                            // get file name
+                            String itemName = item.getName();
+                            String fileName = itemName.substring(itemName.lastIndexOf("\\") + 1);
+                            lsFileName = fileName;
+
+                            String root = getServletContext().getRealPath("/");
+                            File path = new File(root + "/uploads");
+                            if (!path.exists()) {
+                                path.mkdirs();
+                            }
+                            File saveFile = new File(path + "/" + fileName);
+                            item.write(saveFile);
+
+                            System.out.println(saveFile.getAbsolutePath());
+                        } catch (Exception e) {
+                            System.err.println(e.getMessage());
                         }
-                        File uploadedFile = new File(path + "/" + fileName);
-                        System.out.println(uploadedFile.getAbsolutePath());
-                        item.write(uploadedFile);
-                        Topic topic = Topic.builder()
-                                .title(title)
-                                .content(content)
-                                .image(fileName)
-                                .status("pending")
-                                .build();
-                        boolean addTopic = topicDao.addTopicReviewer(topic);
-                        if (!addTopic) {
-                            request.setAttribute("message", "Add Topic Fail!");
-                            request.getRequestDispatcher("topic-reviewer.jsp").forward(request, response);
-                        }
-                        response.sendRedirect("add-topic-reviewer");
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+
+                String title = (String) params.get("title");
+                String content = (String) params.get("content");
+                Topic topic = Topic.builder()
+                        .title(title)
+                        .content(content)
+                        .image(lsFileName)
+                        .status("pending")
+                        .build();
+                boolean addTopic = topicDao.addTopicReviewer(topic);
+                if (!addTopic) {
+                    request.setAttribute("message", "Add Topic Fail!");
+                    request.getRequestDispatcher("topic-reviewer.jsp").forward(request, response);
+                }
+                response.sendRedirect("add-topic-reviewer");
             }
         }
     }
